3c46f32b762e00283016dc3fce070a212e91f0bd
tcoldx <60496088+tcoldx@users.noreply.github.com>
2022-02-26 01:43:17 -0800

    initial-setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "./Home.styles";
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>home</h1>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/components/SideBar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./SideBar.styles";
+
+const SideBar = () => {
+  return (
+    <Container>
+      <div>sidebar</div>
+    </Container>
+  );
+};
+
+export default SideBar;
